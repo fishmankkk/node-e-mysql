703f743602121b47fa39302876e1720ba73c1f69
fishman <609745278@qq.com>
2018-04-23 23:25:20 +0800

    build my project

---
+++ config/conf.js
@@ -0,0 +1,21 @@
+var conf = {
+//   server: {
+//       api: {
+//           host: "127.0.0.1",
+//           port: "8000"
+//       },
+//       res: {
+//           host: "127.0.0.1",
+//           port: "8001"
+//       }
+//   },
+  mysql: {
+    host:"localhost",
+    user:"root",
+    password:"123456",
+    database:"sakila",
+    port:3306
+  }
+};
+
+module.exports = conf;
