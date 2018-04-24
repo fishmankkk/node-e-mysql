36e8e9948bce13c560ed3e6dc3fc3ec44b490a57
XiaoKeng Huang <609745278@qq.com>
2018-04-24 15:30:22 +0800

    增删改查

--- config/conf.js
+++ config/conf.js
@@ -1,18 +1,8 @@
 var conf = {
-//   server: {
-//       api: {
-//           host: "127.0.0.1",
-//           port: "8000"
-//       },
-//       res: {
-//           host: "127.0.0.1",
-//           port: "8001"
-//       }
-//   },
   mysql: {
     host:"localhost",
     user:"root",
-    password:"123456",
+    password:"yu0605k1003",
     database:"sakila",
     port:3306
   }
